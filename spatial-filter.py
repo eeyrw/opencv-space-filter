ca669c48f0a030a3ed3c26f63dc194ace106d763
yuan <ayrw@163.com>
2018-12-06 20:10:34 +0800

    Add some feature.

--- spatial-filter.py
+++ spatial-filter.py
@@ -11,6 +11,15 @@ def addSaltAndPepper(src,percetage):
             NoiseImg[randX,randY]=0
         else:
             NoiseImg[randX,randY]=255          
+    return NoiseImg
+
+def addGaussNoise(src,percetage):
+    NoiseImg=src.copy()
+    NoiseNum=int(percetage*src.shape[0]*src.shape[1])
+    for i in range(NoiseNum):
+        randX=np.random.random_integers(0,src.shape[0]-1)
+        randY=np.random.random_integers(0,src.shape[1]-1)
+        NoiseImg[randX,randY]=np.random.random_integers(0,255)        
     return NoiseImg 
 
 def mediumFilter(src,kernelSize):
@@ -42,13 +51,17 @@ def averageFilter(src,kernelSize):
 
 rawImage=cv2.imread('banana.jpg',flags=cv2.IMREAD_GRAYSCALE)
 cv2.imshow('Raw Image',rawImage)
-noiseImage=addSaltAndPepper(rawImage,0.2)
-cv2.imshow('Noise Image',noiseImage)
-filteredImage=mediumFilter(noiseImage,5)
-cv2.imshow('Medium Filtered Image',filteredImage)
-filteredImageAvg=averageFilter(noiseImage,5)
-cv2.imshow('Average Filtered Image',filteredImageAvg)
-#dst = cv2.GaussianBlur(src=img, ksize=(35, 35), sigmaX=0)
-
+noiseImage=addSaltAndPepper(rawImage,0.1)
+cv2.imshow('Salt and Pepper Noise Image',noiseImage)
+gaussNoiseImage=addGaussNoise(rawImage,0.1)
+cv2.imshow('Gauss Noise Image',gaussNoiseImage)
+filteredImage=mediumFilter(noiseImage,3)
+cv2.imshow('Medium Filtered S&P Noise Image',filteredImage)
+filteredImageAvg=averageFilter(noiseImage,3)
+cv2.imshow('Average Filtered S&P Noise Image',filteredImageAvg)
+filteredGaussNoiseImage=mediumFilter(gaussNoiseImage,3)
+cv2.imshow('Medium Filtered Gauss Noise Image',filteredGaussNoiseImage)
+filteredGaussNoiseImageAvg=averageFilter(gaussNoiseImage,3)
+cv2.imshow('Average Filtered Gauss&P Noise Image',filteredGaussNoiseImageAvg)
 
 cv2.waitKey(0)
